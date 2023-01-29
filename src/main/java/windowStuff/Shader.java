b17d12562ab6d35ea6517388f68ee052478529dd
KarelStehlik <karel.stehlik.cz@gmail.com>
2023-01-29 15:24:31 +0100

    ttt

--- src/main/java/windowStuff/Shader.java
+++ src/main/java/windowStuff/Shader.java
@@ -46,9 +46,13 @@ public class Shader {
       String[] sources = Files.readString(Paths.get(path)).split("#type ");
       for (String source : sources) {
         if (source.startsWith("fragment")) {
-          fragmentSource = source.substring(10);
+          for(int i=0;i<15;i++){
+            System.out.println(Character.getName(source.charAt(i)));
+          }
+          System.out.println();
+          fragmentSource = source.substring(source.indexOf('#'));
         } else if (source.startsWith("vertex")) {
-          vertexSource = source.substring(8);
+          vertexSource = source.substring(source.indexOf('#'));
         }
       }
     } catch (IOException e) {
